d5cdedc92491d2f5e473580f0ed374fbc50e40b7
Diksha. <dikshaupadhyay0203@gmail.com>
2025-07-23 23:48:22 +0530

    updated

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
    
+    # path('',views.base,name='base'),
     path('', views.home_view, name='home'),  
      path("register/",views.register,name='register'),
      path("login/", views.login,name='login'),
      path("logout",views.logout,name="logout"),
+     
 
 ]
