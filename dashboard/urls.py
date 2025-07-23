9e774f0b925179c84d60880d293698251d5b951f
mithun <149506894+looney-blooper@users.noreply.github.com>
2025-07-24 01:24:27 +0530

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
     path('edit-profile', views.edit_profile, name="edit"),
-    path('story/', views.story_generator, name="story_generator"),
 ]
 
