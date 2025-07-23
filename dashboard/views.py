1eb60c44febd9c847108cbbe0696bbe4e070a244
looney-blooper <mithunarulmani017@gmail.com>
2025-07-23 23:06:57 +0530

    ai story generator added

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+# Create your views he
+from .AI_agent.ai_main import main_generate
+from django.conf import settings
+import os
 
 
 @login_required
@@ -8,3 +11,9 @@ def dashboard(request):
 
 
     return render(request,"dashboard/dashboard.html",{'first_name': request.user.first_name })
+
+
+def story_generator(request):
+    static_dir = os.path.join(settings.BASE_DIR, "static")
+    main_generate("A detective duck wearing a detective suit is solving a mystery of an egg", img_path=static_dir)
+    return render(request, "dashboard/about.html", {'first_name': request.user.first_name})
