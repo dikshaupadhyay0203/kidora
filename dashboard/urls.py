23765ac858685f16a0bf7771bbca8dc0f330ae45
looney-blooper <mithunarulmani017@gmail.com>
2025-07-23 23:06:57 +0530

    ai story generator added

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('',views.dashboard,name="dashboard")
+path('',views.dashboard,name="dashboard"),
+path('story/', views.story_generator, name="story_generator"),
 ]
 
