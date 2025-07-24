091960adc50a7caab6d61a58dba77bab91f25a3d
Sushil <sushilksingh2001@gmail.com>
2025-07-24 19:13:15 +0530

    minor changes

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
    
     path('', views.home_view, name='home'),  
-     path("register/",views.register,name='register'),
-     path("login/", views.login,name='login'),
-     path("logout",views.logout,name="logout"),
+     path("register/", views.register, name='register'),
+     path("login/", views.login, name='login'),
+     path("logout", views.logout, name="logout"),
 
 ]
