e4f75b1f40dd9a9fc8aed63b72c72e64d51b1b9c
Sushil <sushilksingh2001@gmail.com>
2025-07-24 14:41:34 +0530

    Feedback form implemented

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from .models import Feedback
 from django.contrib import messages
-# Create your views here.
-# Create your views he
 from .AI_agent.ai_main import main_generate
 from django.conf import settings
 import os
+# Create your views here.
+# Create your views he
 
 
 
@@ -39,6 +40,21 @@ def change_password(request):
           messages.success(request, f"Password changed successfully")
           return redirect('dashboard')
      return render(request, 'dashboard/dashboard.html')
+
+@login_required
+def feedback_form(request):
+     if request.method == "POST":
+        feedback = Feedback(
+             user = request.user,
+             subject= request.POST.get('subject'),
+             message = request.POST.get('message'),
+             feedback_type = request.POST.get('feedback_type')
+        )
+        feedback.save()
+        messages.success(request, f"Feedback Submitted Successfully")
+        return redirect(dashboard)
+     
+     return render(request, 'dashboard/feedback-form.html')
 
 
 
