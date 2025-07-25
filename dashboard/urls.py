615c4f8aad666ce5886187300fdc01d5e9e9eac5
mithun <149506894+looney-blooper@users.noreply.github.com>
2025-07-25 11:23:40 +0530

    Update urls.py

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path('edit-profile', views.edit_profile, name="edit"),
     path('feedback-form', views.feedback_form, name="feedback"),
     path('story-generate', views.story_generate, name="story_generate"),
+    path('rhyme-generator/', views.rhyme_generator, name='rhyme_generator'),
+    path('ask-buddy/', views.ask_buddy, name='ask_buddy'),
     path('quiz/',views.quiz,name="quiz"),
-     path('quiz/complete/', views.quiz_complete, name='quiz_complete'),
+    path('quiz/complete/', views.quiz_complete, name='quiz_complete'),
 
     path('user-profile', views.user_profile, name="user_profile"),
 ]
